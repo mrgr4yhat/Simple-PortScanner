8577627a6657c45482d2e0c00afb852d20ffbb17
Midhun Mohanan <60269061+TheGr4yhAt@users.noreply.github.com>
2020-02-17 20:22:16 +0530

    Update scanner.py

--- scanner.py
+++ scanner.py
@@ -1,17 +1,16 @@
-from socket import *
-import time
-startTime = time.time()
+import socket
 
-if __name__ == '__main__':
-   target = input('Enter the host to be scanned: ')
-   t_IP = gethostbyname(target)
-   print ('Starting scan on host: ', t_IP)
-   
-   for i in range(50, 500):
-      s = socket(AF_INET, SOCK_STREAM)
-      
-      conn = s.connect_ex((t_IP, i))
-      if(conn == 0) :
-         print ('Port %d: OPEN' % (i,))
-      s.close()
-print('Time taken:', time.time() - startTime)
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.settimeout(5)
+
+host = input("Please enter the IP you want to scan: ")
+port = int(input("Please enter the port you want to scan: "))
+
+
+def portScanner(port):
+    if s.connect_ex((host, port)):
+        print("The port is closed")
+    else:
+        print("The port is open")
+
+portScanner(port)
